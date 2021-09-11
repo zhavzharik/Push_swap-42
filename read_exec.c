3f1ea19bcb4c75abfcb1293ecbcfbf77386ef418
Andera Bridger <abridger@mi-j5.msk.21-school.ru>
2021-09-11 22:05:32 +0300

    pointer function

--- read_exec.c
+++ read_exec.c
@@ -6,7 +6,7 @@
 /*   By: abridger <abridger@student.21-school.ru    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/10 17:21:06 by abridger          #+#    #+#             */
-/*   Updated: 2021/09/10 21:54:34 by abridger         ###   ########.fr       */
+/*   Updated: 2021/09/11 21:23:12 by abridger         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	reading_instr(int rd, char **line)
 	char	ch;
 	char	*buffer;
 
-	buffer = malloc(100);
+	buffer = malloc(128);
 	*line = buffer;
 	i = 0;
 	while (rd > 0 && ch != '\n')
@@ -51,23 +51,44 @@ char	**create_array_instr(void)
 	return (instr);
 }
 
+t_action	**create_array_function(t_stack **a, t_stack **b)
+{
+	t_action	*array_func;
+
+	(*array_func) = NULL;
+	array_func = (t_action *)malloc(11 * sizeof(t_action));
+	(*array_func)[sa] = &swap_a;
+	(*array_func)[sb] = &swap_b;
+	(*array_func)[ss] = &swap_twocheck(a, b);
+	(*array_func)[pa] = &push_check(b, a);
+	(*array_func)[pb] = &push_check(a, b);
+	(*array_func)[ra] = &rotate_check(a);
+	(*array_func)[rb] = &rotate_check(b);
+	(*array_func)[rr] = &rotate_twocheck(a, b);
+	(*array_func)[rra] = &rev_rotatecheck(a);
+	(*array_func)[rrb] = &rev_rotatecheck(b);
+	(*array_func)[rrr] = &rev_rotate_twocheck(a, b);
+	return (array_func);
+}
+
 void	get_execute(t_stack **a, t_stack **b)
 {
-	char	*line;
-	char	**instr;
-	int		i;
-	int		rd;
+	char		*line;
+	char		**instr;
+	t_action	**func;
+	int			i;
+	int			rd;
 
 	i = 0;
 	rd = 1;
-	(void)a; // not used yet
-	(void)b; // not used yet
 	instr = create_array_instr();
+	func = create_array_function(a, b);
 	while (reading_instr(rd, &line))
 	{
 		while (instr[i] && ft_strcmp(instr[i], line))
 			i++; // index function with instruction in array of functions
-		//execute instructions;
+		func[i](a, b);//execute instructions;
 	}
-	free(instr); // rewrite and move into checker function
+	free(instr); // rewrite?
+	free(func);
 }
